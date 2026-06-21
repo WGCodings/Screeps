832ca5a631f0b4942f85e16998a190b9db5de187
warre <s0355721@gmail.com>
2026-06-21 17:45:06 +0200

    fork Douwco screepsxx

--- Peabrain/src/Loop.cpp
+++ Peabrain/src/Loop.cpp
@@ -1,8 +1,11 @@
 #include <Screeps/Context.hpp>
 #include <Screeps/Creep.hpp>
+#include <Screeps/StructureSpawn.hpp>
 
 #include <emscripten.h>
 #include <emscripten/bind.h>
+#include <emscripten/val.h>
+
 
 EMSCRIPTEN_KEEPALIVE
 extern "C" void loop()
@@ -12,10 +15,8 @@ extern "C" void loop()
 	JS::console.log(std::string("\n\n\n\n\n\n\n\n\n"));
 	JS::console.log(std::string("Processing tick:\t") + std::to_string(Screeps::Game.time()));
 
-	// Do things here
-
-
 	JS::console.log("Used CPU:\t" + std::to_string(Screeps::Game.cpuGetUsed()));
+	JS::console.log("Bucket:\t" + std::to_string(static_cast<int>(Screeps::Game.cpu()["bucket"])));
 }
 
 EMSCRIPTEN_BINDINGS(loop)
